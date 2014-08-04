1b3e3bc2ba11b340d0b48f40f4420bfa90fd6c08
alan <cmba@qq.com>
2014-08-04 23:39:34 +0800

    user regist and login

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -3,8 +3,31 @@ from flask_wtf import Form
 from wtforms import TextField, PasswordField
 from wtforms.validators import Required,Email,Length, EqualTo
 
+#注册框
 class RegistForm(Form):
 	username = TextField(u"姓名", validators=[Required(u'用户名不能为空')])
 	email = TextField(u"邮箱", validators=[Required(u'邮件不能为空'),Email(u'无效邮址')])
 	password = PasswordField(u"密码", validators=[Length(message=u'长度不能少于6位', min=6 )])
 	repassword = PasswordField(u"密码确认", validators=[EqualTo('password',message=u'口令不匹配')])
+	
+#登录框	
+class LoginForm(Form):
+	username = TextField(u"姓名", validators=[Required(u'用户名不能为空')])
+	password = PasswordField(u"密码", validators=[Length(message=u'长度不能少于6位', min=6 )])
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
